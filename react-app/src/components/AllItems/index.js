e53bdeb15240a0c0eab226bddd3252513b655026
anwersaad0 <anwer.saad0@gmail.com>
2023-06-13 23:36:22 -0700

    added search bar to all listings

--- react-app/src/components/AllItems/index.js
+++ react-app/src/components/AllItems/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllItemsThunk } from "../../store/items";
 import { NavLink } from "react-router-dom";
@@ -10,6 +10,8 @@ function AllItems() {
     const items = useSelector((state) => Object.values(state.items));
     const itemsRev = items.toReversed();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
         dispatch(getAllItemsThunk())
     }, [dispatch]);
@@ -20,21 +22,33 @@ function AllItems() {
     if (!items) return null;
 
     return (
-        <div className="all-items-div">
-            {itemsRev?.map(({name, price, image, rating, id}) => (
-                <NavLink className='card-mini-details' exact to={`/items/${id}`}>
-                    <div className="card-mini-div">
-                        <div className="card-mini-img-div">
-                            <img className="card-mini-img" src={image} alt="Card Image"></img>
+        <div className="all-items-page">
+            <input className="card-search" placeholder="Enter Card Name" onChange={e => setQuery(e.target.value)} />
+
+            {query? (<h2 className="search-text">Search results for {query}</h2>) : ""}
+
+            <div className="all-items-div">
+                {itemsRev?.filter(item => {
+                    if (query === '') {
+                        return item;
+                    } else if (item.name.toLowerCase().includes(query.toLocaleLowerCase())) {
+                        return item;
+                    }
+                }).map(({name, price, image, rating, id}) => (
+                    <NavLink className='card-mini-details' exact to={`/items/${id}`}>
+                        <div className="card-mini-div">
+                            <div className="card-mini-img-div">
+                                <img className="card-mini-img" src={image} alt="Card Image"></img>
+                            </div>
+                            <div className="card-mini-text">
+                                <p className="card-mini-name">{name}</p>
+                                <p className="card-mini-price">Price: ${price.toFixed(2)}</p>
+                                <p className="card-mini-rating">Player Rating: <i className="fas fa-star card-mini-star" /> {(rating) ? rating.toFixed(1) : "Unrated"}</p>
+                            </div>
                         </div>
-                        <div className="card-mini-text">
-                            <p className="card-mini-name">{name}</p>
-                            <p className="card-mini-price">Price: ${price.toFixed(2)}</p>
-                            <p className="card-mini-rating">Player Rating: <i className="fas fa-star card-mini-star" /> {(rating) ? rating.toFixed(1) : "Unrated"}</p>
-                        </div>
-                    </div>
-                </NavLink>
-            ))}
+                    </NavLink>
+                ))}
+            </div>
         </div>
     )
 }
