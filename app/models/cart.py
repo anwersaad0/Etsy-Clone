bdb357544ad88aba603fec91c653d5ddc56aa655
anwersaad0 <anwer.saad0@gmail.com>
2023-06-20 22:03:38 -0700

    fixed typo in cart script

--- app/models/cart.py
+++ app/models/cart.py
@@ -11,6 +11,7 @@ class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     user_id = db.Column(db.Integer, ForeignKey(add_prefix_for_prod('users.id')))
+    
 
     user = db.relationship('User', back_populates='cart')
     items = db.relationship('Item', back_populates='carts')
