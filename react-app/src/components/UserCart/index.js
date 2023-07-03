9d365709bb53e83304b75e3ceea5db2b86dcf781
anwersaad0 <anwer.saad0@gmail.com>
2023-07-03 01:32:19 -0700

    added starting files for a user cart component

---
+++ react-app/src/components/UserCart/index.js
@@ -0,0 +1,9 @@
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { getUserCartThunk } from "../../store/cart";
+
+function UserCart() {
+
+}
+
+export default UserCart;
--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -1,2 +1,34 @@
 const GET_USER_CART = "/GET_USER_CART";
 
+const getUserCartAct = (carts) => {
+    return {
+        type: GET_USER_CART,
+        carts
+    }
+}
+
+export const getUserCartThunk = () => async (dispatch) => {
+    const res = await fetch('/api/carts/current');
+
+    if (res.ok) {
+        const userCart = await res.json();
+        dispatch(getUserCartAct(userCart));
+    }
+}
+
+const initState = {}
+function cartReducer(state = initState, action) {
+    let newState;
+    switch(action.type) {
+        case GET_USER_CART:
+            const userState = action.carts.reduce((userCarts, cart) => {
+                userCarts[cart.userId] = cart;
+                return userCarts; 
+            }, {});
+            return {...userState};
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
